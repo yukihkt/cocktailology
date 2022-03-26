1fb17d8657956495b062c9d4407b94bdb867c7a4
euodiatan <91814774+euodiatan@users.noreply.github.com>
2022-03-26 19:48:21 +0800

    order, place order and api

--- app.py
+++ app.py
@@ -1,66 +1,12 @@
 from flask import Flask, render_template, jsonify, request
+
 import requests
-import paypalrestsdk
 
 app = Flask(__name__)
 
-paypalrestsdk.configure({
-  "mode": "sandbox", # sandbox or live
-  "client_id": "ARInOSQswDDw2JXnDpyAdDTs9Dgdf9tNDBUq-MSC9nRRICQrPXh7p8XiGYsqJlDin-j5oe6ZKN8P-Yrt",
-  "client_secret": "EOOuzw2fQH3aKlrRRNts_mR3nNu_wjuuXShoKKZ_lP3R0KBMndy203bPiBssayygnLqZMH4mV4VR11Cg" })
-
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/home')
 def homepage():
     return render_template('home.html')
-
-@app.route('/payment', methods=['POST'])
-def payment():
-
-    payment = paypalrestsdk.Payment({
-        "intent": "sale",
-        "payer": {
-            "payment_method": "paypal"},
-        "redirect_urls": {
-            "return_url": "http://localhost:3000/payment/execute",
-            "cancel_url": "http://localhost:3000/"},
-        "transactions": [{
-            "item_list": {
-                "items": [{
-                    "name": "testitem",
-                    "sku": "12345",
-                    "price": "500.00",
-                    "currency": "USD",
-                    "quantity": 1}]},
-            "amount": {
-                "total": "500.00",
-                "currency": "USD"},
-            "description": "This is the payment transaction description."}]})
-
-    if payment.create():
-        print('Payment success!')
-    else:
-        print(payment.error)
-
-    return jsonify({'paymentID' : payment.id})
-
-@app.route('/execute', methods=['POST'])
-def execute():
-    success = False
-
-    payment = paypalrestsdk.Payment.find(request.form['paymentID'])
-
-    if payment.execute({'payer_id' : request.form['payerID']}):
-        print('Execute success!')
-        success = True
-    else:
-        print(payment.error)
-
-    return jsonify({'success' : success})
-
 
 @app.route('/about')
 def about():
