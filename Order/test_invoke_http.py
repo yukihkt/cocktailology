11b3c000eeee9dbac049134e98125a82a56c22ab
euodiatan <91814774+euodiatan@users.noreply.github.com>
2022-03-27 23:08:46 +0800

    order update

--- Order/test_invoke_http.py
+++ Order/test_invoke_http.py
@@ -1,6 +1,7 @@
 # test_invoke_http.py
 from invokes import invoke_http
 
+
 # invoke account microservice to get all accounts
 results = invoke_http("http://localhost:5003/order", method='GET')
 
@@ -9,21 +10,8 @@ print()
 print( results )
 
 # invoke order microservice to create an order
-order_id = '2'
-
-order_details = { "account_id": "2", "orderStatus": "NEW", "created": "2022-06-12 02:14:55", "order_item":[{
-            "item_id": 1, 
-            "order_id": 1, 
-            "packageName": "Bloody Mary", 
-            "quantity": 1
-          }, 
-          {
-            "item_id": 2, 
-            "order_id": 1, 
-            "packageName": "Singapore Sling", 
-            "quantity": 1
-          }]}
-
+order_id = '4'
+order_details = {"account_id": 4, "created": "Sun, 12 Jun 2022 10:14:55 GMT", "orderStatus": "NEW", "order_id": 4, "cart_item": [{"item_id": 8, "order_id": 4, "packageName": "Drink1", "quantity": 1}, {"item_id": 9, "order_id": 4, "packageName": "Drink2", "quantity": 1}]}
 create_results = invoke_http(
         "http://localhost:5003/order" + "/" + order_id, method='POST', 
         json=order_details
